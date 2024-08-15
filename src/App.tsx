720302163c52ba8e881cf11de1719e753c5e0519
nghyane <hoangvananhnghia99@gmail.com>
2024-08-15 20:41:55 +0700

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import "src/css/App.css";
 
 import HelloWorld from "components/HelloWorld";
 
-const App: React.FC = () => <HelloWorld />;
+
+const BrowserRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <HelloWorld />,
+    }
+]);
+
+
+
+const App: React.FC = () => {
+    return (
+        <RouterProvider router={BrowserRouter} />
+    );
+}
+
 
 export default App;
